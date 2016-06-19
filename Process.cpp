003743c8492589153157a39d744eea819189dcc6
Lam Dang <lam.dv@live.com>
2016-06-19 11:01:36 +0700

    Process object

--- Process.cpp
+++ Process.cpp
@@ -1,7 +1,42 @@
+#include <time.h>
+
 class Process
 {
 	private:
-		string p_id;
+		int p_id;
+		time_t time; 
+		string CMD;
 	public:
-
+		void Process(int p_id, time_t time, string CMD)
+		{
+			this.p_id = p_id;
+			this.time = time;
+			this.CMD = CMD;
+		}
+		
+		int get_P_id()
+		{
+			return this.p_id;
+		}
+		int get_time()
+		{
+			return this.time;
+		}
+		int get_CMD()
+		{
+			return this.CMD;
+		}
+		
+		void set_P_id(int p_id)
+		{
+			this.p_id = p_id;
+		}
+		void set_time(time_t time)
+		{
+			this.time = time;
+		}
+		void set_CMD(string CMD)
+		{
+			this.CMD = CMD;
+		}
 };

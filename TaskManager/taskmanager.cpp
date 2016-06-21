e3e6dca512c5347b3ec8fbd5d6ccfbad0112a788
ngocankl95 <ngocan.4595@gmail.com>
2016-06-21 15:20:11 +0700

    part 1

--- TaskManager/taskmanager.cpp
+++ TaskManager/taskmanager.cpp
@@ -2,102 +2,56 @@
 #include "ui_taskmanager.h"
 #include "warning.h"
 
-TaskManager::TaskManager(QWidget *parent) :
-    QWidget(parent),
+TaskManager::TaskManager(QWidget *parent) : QWidget(parent),
     ui(new Ui::TaskManager)
 {
     ui->setupUi(this);
+    int last_time;
+    QDateTime current = QDateTime::currentDateTime();
+
+
 
 //Set table
-    QStringList title;
 
 //Set main win title
     setWindowTitle("Task Manager");
+    //periodically refresh table
+    if(current.toTime_t() - last_time > 1)
+    {
+        fillTable();
+        last_time = current.toTime_t();
+    }
 
-//Create table
+      return;
+}
+
+void TaskManager::fillTable(){
+    ui->tableWidget->clear();
+    ui->tableWidget->setRowCount(0);
     ui -> tableWidget -> setColumnCount(11);
+    QStringList title;
     title << "User" << "PID" << "CPU Usage" << "Memory Usage" << "VSZ" << "RSS" << "TTY" << "STAT" << "START" << "Running time" << "Command";
     ui -> tableWidget -> setHorizontalHeaderLabels(title);
-
-//Create row
-    ui -> tableWidget -> insertRow(ui -> tableWidget -> rowCount());
-    file = ui -> tableWidget -> rowCount() - 1;
-
-//Show running processes in terminal
+    ui -> tableWidget->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui -> tableWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
     FILE *fp;
-    char path[1035];
+    char path[10000];
 
     /* Open the command for reading. */
     fp = popen("ps aux", "r");
     if (fp == NULL) {
-        cout << "Failed to run command\n";
+        std::cout << "Failed to run command\n";
         exit(1);
     }
-
+    /* Skip the first line */
+    fgets(path, sizeof(path)-1, fp);
     /* Read the output a line at a time - output it. */
     while (fgets(path, sizeof(path)-1, fp) != NULL) {
-        void TaskManager::AddFile(string s){
-            string t_user, t_tty, t_stat, t_start, t_time, t_cmd, t_cpu, t_mem;
-            int t_id, t_vsz, t_rss;
-
-            //get user
-            s = s.substr(mark+1);
-            mark = s.find("\t");
-            t_user = s.substr(0,mark);
-
-            //Get id
-            int mark = s.find("\t");
-            t_id = atoi(s.substr(0,mark).c_str());
-
-            //Get cpu
-            s = s.substr(mark+1);
-            mark = s.find("\t");
-            t_cpu = s.substr(0,mark);
-
-            //Get mem
-            s = s.substr(mark+1);
-            mark = s.find("\t");
-            t_mem = s.substr(0,mark);
-
-            //Get vsz
-            int mark = s.find("\t");
-            t_vsz = atoi(s.substr(0,mark).c_str());
-
-            //Get rss
-            int mark = s.find("\t");
-            t_rss = atoi(s.substr(0,mark).c_str());
-
-            //Get TTY
-            s = s.substr(mark+1);
-            mark = s.find("\t");
-            t_tty = s.substr(0,mark);
-
-            //Get STAT
-            s = s.substr(mark+1);
-            mark = s.find("\t");
-            t_stat = s.substr(0,mark);
-
-            //Get START
-            s = s.substr(mark+1);
-            mark = s.find("\t");
-            t_start = s.substr(0,mark);
-
-            //Get time
-            s = s.substr(mark+1);
-            mark = s.find("\t");
-            t_time = s.substr(0,mark);
-
-            //Get command
-            s = s.substr(mark+1);
-            mark = s.find("\t");
-            t_cmd = s.substr(0,mark);
-        }
+        AddFile(path);
     }
 
       /* close */
       pclose(fp);
-
-      return 0;
 }
 
 TaskManager::~TaskManager()
@@ -109,6 +63,8 @@ TaskManager::~TaskManager()
 void TaskManager::on_endProcess_clicked()
 {
     int res;
+    QString command;
+    QItemSelectionModel *select = ui->tableWidget->selectionModel();
     WARNING wn(this);
 
 //Set sub win title
@@ -116,7 +72,95 @@ void TaskManager::on_endProcess_clicked()
 
     res = wn.exec();
 
-//If cancel kill
-    if(res == QDialog::Rejected)
-        return;
+    if((res != QDialog::Rejected)&&(!select->selectedRows(1).isEmpty()))
+    {
+        command = "kill " + select->selectedRows(1).takeFirst().data().toString();
+        system(command.toStdString().c_str());
+        system("wait");
+
+    }
+
+    fillTable();
+}
+
+void TaskManager::AddFile(QString s){
+    //Create row
+        ui -> tableWidget -> insertRow(ui -> tableWidget -> rowCount());
+        int file = ui -> tableWidget -> rowCount() - 1;
+
+    QString t_user, t_tty, t_stat, t_start, t_time, t_cmd, t_cpu, t_mem;
+    int t_id, t_vsz, t_rss;
+
+    //get user
+    QStringList mark = s.split(" ", QString::SkipEmptyParts);
+//    s = s.substr(mark+1);
+//    t_user = s.substr(0,mark);
+    t_user = mark[0];
+
+    //Get id
+//    mark = s.split("\t");
+//    t_id = atoi(mark[1].c_str());
+    t_id = mark[1].toInt();
+
+    //Get cpu
+//    s = s.substr(mark+1);
+//    mark = s.split("\t");
+    t_cpu = mark[2];
+
+    //Get mem
+//    s = s.substr(mark+1);
+//    mark = s.split("\t");
+    t_mem = mark[3];
+
+    //Get vsz
+//    mark = s.split("\t");
+//    t_vsz = atoi(mark[4].c_str());
+    t_vsz = mark[4].toInt();
+
+    //Get rss
+//    mark = s.split("\t");
+//    t_rss = atoi(mark[5].c_str());
+    t_rss = mark[5].toInt();
+
+    //Get TTY
+//    s = s.substr(mark+1);
+//    mark = s.split("\t");
+    t_tty = mark[6];
+
+    //Get STAT
+//    s = s.substr(mark+1);
+//    mark = s.split("\t");
+//    t_stat = s.substr(0,mark);
+    t_stat = mark[7];
+
+    //Get START
+//    s = s.substr(mark+1);
+//    mark = s.split("\t");
+//    t_start = s.substr(0,mark);
+    t_start = mark[8];
+
+    //Get time
+//    s = s.substr(mark+1);
+//    mark = s.split("\t");
+//    t_time = s.substr(0,mark);
+    t_time = mark[9];
+
+    //Get command
+//    s = s.substr(mark+1);
+//    mark = s.split("\t");
+//    t_cmd = s.substr(0,mark);
+    t_cmd = mark[10];
+
+    //Insert row
+    ui -> tableWidget -> setItem(file, User, new QTableWidgetItem(t_user));
+    ui -> tableWidget -> setItem(file, PID, new QTableWidgetItem(QString::number(t_id)));
+    ui -> tableWidget -> setItem(file, CPU_Usage, new QTableWidgetItem(t_cpu));
+    ui -> tableWidget -> setItem(file, Memory_Usage, new QTableWidgetItem(t_mem));
+    ui -> tableWidget -> setItem(file, VSZ, new QTableWidgetItem(QString::number(t_vsz)));
+    ui -> tableWidget -> setItem(file, RSS, new QTableWidgetItem(QString::number(t_rss)));
+    ui -> tableWidget -> setItem(file, TTY, new QTableWidgetItem(t_tty));
+    ui -> tableWidget -> setItem(file, STAT, new QTableWidgetItem(t_stat));
+    ui -> tableWidget -> setItem(file, START, new QTableWidgetItem(t_start));
+    ui -> tableWidget -> setItem(file, Running_time, new QTableWidgetItem(t_time));
+    ui -> tableWidget -> setItem(file, Command, new QTableWidgetItem(t_cmd));
 }
--- TaskManager/taskmanager.h
+++ TaskManager/taskmanager.h
@@ -2,6 +2,8 @@
 #define TASKMANAGER_H
 
 #include <QWidget>
+#include <iostream>
+#include <qdatetime.h>
 
 namespace Ui {
 class TaskManager;
@@ -14,13 +16,18 @@ class TaskManager : public QWidget
 public:
     explicit TaskManager(QWidget *parent = 0);
     ~TaskManager();
-    void AddFile(string s);
+    void AddFile(QString s);
+    void fillTable();
 
 private slots:
     void on_endProcess_clicked();
 
 private:
     Ui::TaskManager *ui;
+
+    enum Colume {
+        User, PID, CPU_Usage, Memory_Usage, VSZ, RSS, TTY, STAT, START, Running_time, Command
+    };
 };
 
 #endif // TASKMANAGER_H
